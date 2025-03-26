0acaa07cea7170535a8b6411fd928ee1cbd26689
BLK LUV [org] <info@blkluv.org>
2025-03-25 20:30:44 -0400

    Update videolinks.js

--- src/components/videolinks.js
+++ src/components/videolinks.js
@@ -22,76 +22,15 @@ const videolinks = [
   },
   {
     id: 4,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeicsvnequttoldc4hmirfa22rqo2kry4o2pvhbnpy7t4cqvnhzv4na",
+    src: "https://gateway.pinata.cloud/ipfs/bafybeigfntltca2bluj56gshxt5qqq6ozivx2avji7tszasuld2p5i55qq",
     tags: [
-      "#meltdown",
-      "#comedy",
-      "#fun",
-      "#laughs",
-      "#relatable",
-      "#breakdown",
-      "#hilarious",
+      "#zazamemes",
     ],
   },
   {
     id: 5,
     src: "https://gateway.pinata.cloud/bafybeibyf7ssqk7en62lpczqhhq4y7omeginwcu7kb5e6lblfa2zdvnf7y",
     tags: [
-      "#funny",
-    ],
-  },
-  {
-    id: 6,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeibqbdv4hj6nqvkbgdzcxn6o5myiotyrfqaycinojzn562bglt5bpa",
-    tags: [
-      "#casinow3w",
-    ],
-  },
-  {
-    id: 7,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeifdyovsqwkifdh6imezfg7fcbnet4vm3aplcihdsrkt2txzgrh2by",
-    tags: [
-      "#casinow3w",
-    ],
-  },
-  {
-    id: 8,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeiaqn6b6vygqiqvz2lxdhpscpxnlz4kzbgrfdyu6tzsrbiflnncwpe",
-    tags: [
-      "#saratogacasino",
-      "#casino",
-    ],
-  },
-  {
-    id: 9,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeihky6vr6fcyvb3esx45sv52swt5tdjukx72eqvaekbnsh52pluq6e",
-    tags: [
-      "#funny",
-      "#casinow3w",
-    ],
-  },
-  {
-    id: 10,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeiauc2sbdev3wplq3azjjljcst2ig6jgoyevzvh7ukwo3oeg6wnmju",
-    tags: [
-      "#casino",
-      "#casinotiktok",
-    ],
-  },
-  // Repeating URLs with incremented IDs
-  {
-    id: 11,
-    src: "https://gateway.pinata.cloud/ipfs/bafybeigvvcpgoqbqcinruacw273uridu6xyyur4eoyitqt7n7wkivnnze4",
-    tags: [
-      "#eddiemurphy",
-      "#casino",
-    ],
-  },
-  {
-    id: 12,
-    src: "https://gateway.pinata.cloud/ipfs/2e5b6672-d3b3-4ffe-bf91-8b98c68feba6",
-    tags: [
-      "#casino",
       "#funny",
     ],
   },
